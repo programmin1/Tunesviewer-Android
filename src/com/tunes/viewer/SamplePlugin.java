892e673d4f6b15470154b56114bbbbdfa423d479
LBryan <none@none>
2012-01-21 12:41:09 -0800

    add file

---
+++ src/com/tunes/viewer/SamplePlugin.java
@@ -0,0 +1,21 @@
+package com.tunes.viewer;
+
+import android.app.Service;
+
+import android.content.Intent;
+
+import android.os.IBinder;
+
+public class SamplePlugin extends Service {
+
+    @Override
+
+    public IBinder onBind(Intent intent) {
+
+        // TODO Auto-generated method stub
+
+        return null;
+
+    }
+
+}
