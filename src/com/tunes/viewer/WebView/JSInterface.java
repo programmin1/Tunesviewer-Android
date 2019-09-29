6a40ced88c5d01404c79202199566f0c4a603be4
Animesh Verma <aniverma10@gmail.com>
2019-09-29 16:39:34 +0530

    [v1.3.1] Updated Notifier class and increased app target SDK

--- src/com/tunes/viewer/WebView/JSInterface.java
+++ src/com/tunes/viewer/WebView/JSInterface.java
@@ -31,7 +31,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.text.ClipboardManager;
 import android.util.Log;
 import android.widget.TextView;
